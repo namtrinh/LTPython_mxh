cf0f65d811e0734f826090dfc832b654ab999956
Trí Nguyễn <166617014+freddycartman@users.noreply.github.com>
2024-11-12 22:40:57 +0700

    Update models for video upload.py

--- socialmedia/userauth/models.py
+++ socialmedia/userauth/models.py
@@ -39,7 +39,8 @@ class Profile(models.Model):
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images')
+    image = models.ImageField(upload_to='post_images', blank=True, null=True)  # Thêm blank=True, null=True
+    video = models.FileField(upload_to='post_videos', blank=True, null=True)  # Cho phép video trống
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
